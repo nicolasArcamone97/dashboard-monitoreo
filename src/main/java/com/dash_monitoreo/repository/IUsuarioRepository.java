7010d9074eff6e0990c4ad5f19a40a8fcc15cf18
nicolasArcamone97 <109641395+nicolasArcamone97@users.noreply.github.com>
2024-11-11 00:02:44 -0300

    validaciones y excepciones de usuarios controllers y services.

--- src/main/java/com/dash_monitoreo/repository/IUsuarioRepository.java
+++ src/main/java/com/dash_monitoreo/repository/IUsuarioRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario, Long> {
 
+    public Usuario findByEmail(String email);
+
 }
